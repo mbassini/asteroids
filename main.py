01c1ab1d67c0676cc0a0a804d0514e60ce419b77
Mauro <mbassini@outlook.com>
2024-12-02 15:26:09 -0300

    MVP

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting asteroids!")
@@ -9,13 +14,50 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    shots_group = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
+    AsteroidField.containers = (updatable_group)
+    Shot.containers = (shots_group, updatable_group, drawable_group)
+
+    centered_x = SCREEN_WIDTH / 2
+    centered_y = SCREEN_HEIGHT / 2
+    player = Player(centered_x, centered_y)
+
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (1, 1, 1))
+        screen.fill("black")
+
+        for i in updatable_group:
+            i.update(dt)
+
+        for i in drawable_group:
+            i.draw(screen)
+        
+        for asteroid in asteroids_group:
+            if asteroid.is_colliding(player):
+                sys.exit("Game Over!")
+
+            for bullet in shots_group:
+                if asteroid.is_colliding(bullet):
+                    asteroid.split()
+                    bullet.kill()
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
